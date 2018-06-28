dc08a3468af6b6aff2b469977384188108bcb9d0
Marquis Kurt <software@marquiskurt.net>
2018-06-27 21:14:33 -1000

    Update conf to include prohect and copyrights

--- conf.py
+++ conf.py
@@ -6,5 +6,9 @@ source_parsers = {
 
 source_suffix = ['.rst', '.md']
 
+project = u'AliceOS'
+copyright = u'2018 AliceOS Developers'  # pylint: disable=redefined-builtin
+author = u'AliceOS Developers'
+
 def setup(app):
     app.add_stylesheet('custom.css')
