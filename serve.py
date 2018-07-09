b5a305511d4db2e6ded9dd95ec855524dabe3e2a
Marquis Kurt <software@marquiskurt.net>
2018-07-08 16:38:06 -1000

    Update serve to include either build or local tests

--- serve.py
+++ serve.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python
 
+from shutil import copyfile
 import os
+import sys
 
-print "Attempting to delete any existing builds before running..."
+def run_build():
+    print "Attempting to delete any existing builds before running..."
 
-if os.path.exists("build"):
-    os.system("rm -rf build")
-else:
-    pass
+    if os.path.exists("docs"):
+        os.system("rm -rf docs")
+    else:
+        pass
 
-print "Building documentation with documentation-builder..."
-try:
-    os.system("documentation-builder --template template.html --output-path ./docs")
-except:
-    print "Something went wrong. Are you sure you have documentation-builder installed?"
+    print "Building documentation with documentation-builder..."
+    try:
+        os.system("documentation-builder --template template.html --output-path ./docs")
+    except:
+        print "Something went wrong. Are you sure you have documentation-builder installed?"
 
-print "Attempting to serve content locally..."
+    print "Copying GitHub Pages files over..."
+    try:
+        copyfile(".htaccess", "docs/.htaccess")
+        with open("docs/CNAME", "w+") as cname:
+            cname.write("docs.aliceos.app")
+    except:
+        "Something went wrong. Are you sure that you have the right files?"
 
-if os.path.exists("build"):
-    os.chdir("build")
-    print "The content will now be served."
-    print "Press CTRL+C to quit the serving process."
-    os.system("python -m SimpleHTTPServer")
-else:
-    print "There's no build directory, silly."
+def start_serve(action):
+    os.system("clear")
+    if action == "build-only":
+        run_build()
+        print "Build completed."
+    elif action == "build-local-test":
+        run_build()
+        print "Attempting to serve content locally..."
 
-print "All done!"
+        if os.path.exists("docs"):
+            os.chdir("docs")
+            print "The content will now be served."
+            print "Press CTRL+C to quit the serving process."
+            os.system("python -m SimpleHTTPServer")
+        else:
+            print "There's no build directory, silly."
+
+        print "All done!"
+        print "Make sure the docs folder remains when you push to GitHub!"
+    else:
+        print "Error: Argument " + action + " not recognized."
+
+if __name__ == "__main__":
+    start_serve(sys.argv[1])
